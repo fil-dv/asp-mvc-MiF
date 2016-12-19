131c80aa31a72a839742475bd7ad7e233656c038
fil-eadr <d.filonenko@eadr.com.ua>
2016-12-19 14:17:58 +0200

    added songs list to DropDownList

---
+++ MiFWebSolution/Web_UI/Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_UI.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Register()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult Login()
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
