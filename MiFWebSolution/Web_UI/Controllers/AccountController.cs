158f7b66baca28454937d854908258b53822912e
fil-eadr <d.filonenko@eadr.com.ua>
2017-01-16 17:01:59 +0200

    Add registeration view

--- MiFWebSolution/Web_UI/Controllers/AccountController.cs
+++ MiFWebSolution/Web_UI/Controllers/AccountController.cs
@@ -3,15 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web_UI.Models.Auth;
 
 namespace Web_UI.Controllers
 {
     public class AccountController : Controller
     {
         // GET: Account
+        [AllowAnonymous]
         public ActionResult Register()
         {
-            return RedirectToAction("Index", "Home");
+            return View();
+            //return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return View();
+            //return RedirectToAction("Index", "Home");
         }
 
         public ActionResult Login()
---
+++ MiFWebSolution/Web_UI/Models/Auth/RegisterModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Web_UI.Models.Auth
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(255, ErrorMessage = "Минимум 3 символa", MinimumLength = 3)]
+        [Display(Name = "Логин:")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(255, ErrorMessage = "Минимум 7 символов", MinimumLength = 7)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль:")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Обязательное поле")]
+        [StringLength(255, ErrorMessage = "Минимум 7 символов", MinimumLength = 7)]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        [Display(Name = "Подтверждение:")]
+        public string PasswordRepeat { get; set; }
+    }
+}
